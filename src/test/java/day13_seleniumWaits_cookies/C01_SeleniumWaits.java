54b46aca60165d3f7a6934db39c28f53c89df7f6
MehmetÖzdemir <mehmet.ozzdemir@hotmail.com>
2024-06-19 21:13:06 +0300

    T141_JUnit day12_seleniumWaits_cookies  done

---
+++ src/test/java/day13_seleniumWaits_cookies/C01_SeleniumWaits.java
@@ -0,0 +1,18 @@
+package day13_seleniumWaits_cookies;
+
+import org.junit.jupiter.api.Test;
+
+public class C01_SeleniumWaits {
+
+
+
+   @Test
+   public void implicitlyWaitTest(){
+
+
+
+
+   }
+
+
+}
