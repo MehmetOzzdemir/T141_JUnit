5f189435ce3cfb779b6cccf368c1352612c70f5b
MehmetÖzdemir <mehmet.ozzdemir@hotmail.com>
2024-06-20 02:24:52 +0300

    T141_JUnit day14_webTables  done

---
+++ src/test/java/day14_webTables/C01_ClassicHTMLWebTable.java
@@ -0,0 +1,33 @@
+package day14_webTables;
+
+import org.junit.jupiter.api.Test;
+import utilities.TestBase;
+
+public class C01_ClassicHTMLWebTable extends TestBase {
+
+
+    @Test
+    public void test01(){
+        //1."https://testotomasyonu.com/webtables" adresine gidin
+        driver.get("https://testotomasyonu.com/webtables");
+
+        //2.Web table tum body’sini yazdirin
+
+        //3. Web tablosunda "Comfortable Gaming Chair" bulundugunu test edin
+
+        //4. Web table’daki satir sayisinin 5 oldugunu test edin
+
+        //5. Tum satirlari yazdirin
+
+        //6. Web table’daki sutun sayisinin 4 olduğunu test edin
+
+        //7. 3.sutunu yazdirin
+
+        //8. Tablodaki basliklari yazdirin
+
+        //9. Satir ve sutunu parametre olarak alip, hucredeki bilgiyi döndüren bir method olusturun
+
+        //10. 4.satirdaki category degerinin "Furniture" oldugunu test edin
+    }
+
+}
